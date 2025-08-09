7dda985c567518b019d2049ffb238011adb498e4
Jakkanikeerthana <2420030423@klh.edu.in>
2025-08-09 10:31:15 +0530

    Add files via upload

---
+++ exampl3.java
@@ -0,0 +1,11 @@
+package oops;
+
+public class exampl3 {
+	public static void main(String[] args) {
+		{
+			System.out.print("Java\n"); 
+			System.out.print("Python\n");
+			System.out.print("LINUX/UNIX");		
+		}
+	}
+}
