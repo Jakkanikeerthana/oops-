34e588ad91fe5117c2660e25eda5f7d589b5161f
Jakkanikeerthana <2420030423@klh.edu.in>
2025-08-09 10:30:54 +0530

    Add files via upload

---
+++ exampl2.java
@@ -0,0 +1,15 @@
+package oops;
+
+public class exampl2 {
+	public static void main(String args[]) 
+	{
+	    int x; 
+	    for(x = 0; x < 3; x++)
+	       {
+		 int y = -1;  				
+		 System.out.println("y is: " + y); 
+		 System.out.println("y is now: " + y);
+	        }
+         }
+
+}
