aec0fab57abbbc6adca6e8522c478be5d9e548f3
Jakkanikeerthana <2420030423@klh.edu.in>
2025-08-09 10:30:29 +0530

    Add files via upload

---
+++ exampl1.java
@@ -0,0 +1,13 @@
+package oops;
+
+public class exampl1 {
+   public static void main(String[] args) {
+		    String s1 = "abista";
+			String s2 = "abista";
+			System.out.println(s1 == s2); 
+			s1 = s1 + "lakshmi";
+			System.out.println(s1 == s2); 
+			String s3 = "abistalakshmi";
+			System.out.println(s1==s3);
+	}
+}
