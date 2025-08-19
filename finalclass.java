b62ef1a8bbdca08f6979177b8e0bc8123d3bfeb3
Jakkanikeerthana <2420030423@klh.edu.in>
2025-08-19 14:25:40 +0530

    Add files via upload

---
+++ finalclass.java
@@ -0,0 +1,18 @@
+package oop;
+
+public class finalclass {
+
+	public static void main(String[] args) {
+		int x = 10;           
+        final int y = 20;     
+
+        System.out.println("x is: " + x);
+        System.out.println("y is: " + y);
+
+        x = 30;               
+       // y = 40;            
+
+       System.out.println("x is: " + x);
+        System.out.println("y is: " + y);
+	    }
+}
