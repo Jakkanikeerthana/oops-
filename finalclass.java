e5bf6a1dbc9e0484f692fb6dd90c047a7608e635
Jakkanikeerthana <2420030423@klh.edu.in>
2025-08-19 13:48:24 +0530

    Add files via upload

---
+++ finalclass.java
@@ -0,0 +1,18 @@
+
+public class finalclass {
+    public void display()
+    {
+    	System.out.println("hi");
+    }
+}
+public class Demo1 extends finalclass
+{
+	
+	public static void main(String[] args) {
+			finalclass f=new finalclass();
+			f.display();
+		
+
+	}
+
+}
